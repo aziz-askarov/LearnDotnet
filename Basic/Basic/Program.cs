41a10321c741a443672a078591f9d26a38c7518b
aziz-askarov <azizasqarov773@gmail.com>
2026-02-02 10:15:47 +0500

    Initial project setup

---
+++ Basic/Basic/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello, World!");
+
+int age;
+string name =string.Empty;
+
+Console.Write("ism: ");
+name = Console.ReadLine();
+
+Console.Write("yosh: ");
+age = int.Parse(Console.ReadLine());
+
+Console.WriteLine("________User Info_______");
+Console.WriteLine($"Ism: {name}");
+Console.WriteLine($"Yosh: {age}");
