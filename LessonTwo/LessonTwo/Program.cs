36f4946b49692f872062c9e28eb709556a0c1450
aziz-askarov <azizasqarov773@gmail.com>
2026-02-02 11:42:33 +0500

    array

--- LessonTwo/LessonTwo/Program.cs
+++ LessonTwo/LessonTwo/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿var arrs = new int[][]
+{
+    new int[] { 1, 2,3 },
+    new int[] { 1, 2 ,3,5},
+    new int[] { 1, 2 ,6,7,8,9},
+};
+
+
+for (int i = 0; i < arrs.Length; i++)
+{
+    Console.WriteLine("Guruh element soni: " + arrs[i].Length);
+
+    for(int j = 0; j < arrs[i].Length; j++)
+    {
+        Console.WriteLine(arrs[i][j].ToString());
+    }
+}
