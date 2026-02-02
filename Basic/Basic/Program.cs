2263ba7fdd306691f7b72dd9d361305854859094
aziz-askarov <azizasqarov773@gmail.com>
2026-02-02 10:19:10 +0500

    Add

--- Basic/Basic/Program.cs
+++ Basic/Basic/Program.cs
@@ -1,14 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, World!");
 
-int age;
-string name =string.Empty;
+//int age;
+//string name =string.Empty;
 
-Console.Write("ism: ");
-name = Console.ReadLine();
+//Console.Write("ism: ");
+//name = Console.ReadLine();
 
-Console.Write("yosh: ");
-age = int.Parse(Console.ReadLine());
+//Console.Write("yosh: ");
+//age = int.Parse(Console.ReadLine());
 
-Console.WriteLine("________User Info_______");
-Console.WriteLine($"Ism: {name}");
-Console.WriteLine($"Yosh: {age}");
+//Console.WriteLine("________User Info_______");
+//Console.WriteLine($"Ism: {name}");
+//Console.WriteLine($"Yosh: {age}");
+
+Console.WriteLine("2 ta son kiriting");
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+if (x == y )
+{
+    Console.WriteLine("Sonlar teng");
+}
+else if (x > y)
+{
+    Console.WriteLine(x);
+}
+else
+{
+    Console.WriteLine(y);
+}
