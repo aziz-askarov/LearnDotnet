a641dcc0aeddb4162e55fcdd09d03d91b742d122
azizaskarovdev <azizbek.asqarov@webase.uz>
2026-05-08 10:27:52 +0500

    asas

---
+++ AutoTest.Web/AutoTest.Web/Controllers/TicketsController.cs
@@ -0,0 +1,24 @@
+﻿using AutoTest.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace AutoTest.Web.Controllers;
+
+public class TicketsController : Controller
+{
+    private readonly List<QuestionModel>? _questions;
+    public TicketsController()
+    {
+
+        var path = Path.Combine("JsonData", "uzkiril.json");
+        var json = System.IO.File.ReadAllText(path);
+
+        _questions = JsonConvert.DeserializeObject<List<QuestionModel>>(json);
+    }
+
+    public IActionResult Index()
+    {
+        ViewBag.TicketCount = _questions?.Count / 20;
+        return View();
+    }
+}
